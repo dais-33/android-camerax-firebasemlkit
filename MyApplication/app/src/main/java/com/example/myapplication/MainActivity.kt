015bda4ace71313aeb15280de8b728b698ded4ef
dai-33 <holy-dream52@hotmail.co.jp>
2019-05-29 10:51:52 +0900

    物体の認識を行う

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,14 +12,18 @@ import android.util.Size
 import android.view.Surface
 import android.view.TextureView
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.myapplication.view.GraphicOverlay
+import com.example.myapplication.view.ObjectGraphic
 import com.example.myapplication.view.TextGraphic
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.objects.FirebaseVisionObject
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions
 import com.google.firebase.ml.vision.text.FirebaseVisionText
 
 // This is an arbitrary number we are using to keep tab of the permission
@@ -38,6 +42,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var textureView: TextureView
     private lateinit var graphicOverlay: GraphicOverlay
+    private lateinit var radioGroup: RadioGroup
     private var textureSize = Size(0, 0)
     private val handler = Handler()
     private val runnable = object : Runnable {
@@ -45,7 +50,7 @@ class MainActivity : AppCompatActivity() {
             if (ableRunning) {
                 analyzeView()
             }
-            handler.postDelayed(this, 1000L)
+            handler.postDelayed(this, 500L)
         }
     }
     private var ableRunning = false
@@ -57,6 +62,7 @@ class MainActivity : AppCompatActivity() {
 
         textureView = findViewById(R.id.view_finder)
         graphicOverlay = findViewById(R.id.graphic_overlay)
+        radioGroup = findViewById(R.id.radio_group)
 
         // Request camera permissions
         if (allPermissionsGranted()) {
@@ -129,6 +135,9 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    /**
+     * カメラを起動する
+     */
     private fun startCamera() {
         Log.d(TAG, "[startCamera]")
 
@@ -199,6 +208,85 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
+     * カメラの画像を解析する
+     */
+    private fun analyzeView() {
+        Log.d(TAG, "[analyzeView]")
+
+        if (textureView.isAvailable) {
+            val original = textureView.bitmap
+
+            val matrix = Matrix().apply {
+                val displayDegree = when(textureView.display.rotation) {
+                    Surface.ROTATION_0 -> 0
+                    Surface.ROTATION_90 -> 90
+                    Surface.ROTATION_180 -> 180
+                    Surface.ROTATION_270 -> 270
+                    else -> 0
+                }
+
+                postScale(1f / original.width, 1f / original.height)
+                postTranslate(-0.5f, -0.5f)
+                postRotate(-displayDegree.toFloat())
+                postScale(textureView.width.toFloat(), textureView.height.toFloat())
+            }
+            val bitmap = Bitmap.createBitmap(original, 0, 0, original.width, original.height, matrix, true)
+
+            val visionImage = FirebaseVisionImage.fromBitmap(bitmap)
+
+            when(radioGroup.checkedRadioButtonId) {
+                R.id.radio_text -> { analyzeText(visionImage) }
+                R.id.radio_object -> { analyzeObject(visionImage) }
+                R.id.radio_face -> { analyzeFace(visionImage) }
+            }
+        }
+    }
+
+    /**
+     * テキスト認識を行う
+     *
+     * @param visionImage 解析対象の画像
+     */
+    private fun analyzeText(visionImage: FirebaseVisionImage) {
+        Log.d(TAG, "[analyzeText] visionImage=$visionImage")
+
+        val textRecognizer = FirebaseVision.getInstance().onDeviceTextRecognizer
+        textRecognizer.processImage(visionImage)
+            .addOnSuccessListener { texts -> processTextRecognitionResult(texts) }
+            .addOnFailureListener { e -> e.printStackTrace() }
+    }
+
+    /**
+     * 物体認識を行う
+     *
+     * @param visionImage 解析対象の画像
+     */
+    private fun analyzeObject(visionImage: FirebaseVisionImage) {
+        Log.d(TAG, "[analyzeObject] visionImage=$visionImage")
+
+        val options = FirebaseVisionObjectDetectorOptions.Builder()
+            .setDetectorMode(FirebaseVisionObjectDetectorOptions.SINGLE_IMAGE_MODE)
+            .enableMultipleObjects()
+            .enableClassification()
+            .build()
+        val detector = FirebaseVision.getInstance().getOnDeviceObjectDetector(options)
+        detector.processImage(visionImage)
+            .addOnSuccessListener { objectList -> processObjectRecognitionResult(objectList) }
+            .addOnFailureListener { e -> e.printStackTrace() }
+    }
+
+    /**
+     * 顔認識を行う
+     *
+     * @param visionImage 解析対象の画像
+     */
+    private fun analyzeFace(visionImage: FirebaseVisionImage) {
+        Log.d(TAG, "[analyzeFace] visionImage=$visionImage")
+
+
+    }
+
+    /**
      * テキスト認識結果を画面に表示する
      *
      * @param result テキスト認識結果
@@ -222,35 +310,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun analyzeView() {
-        Log.d(TAG, "[analyzeView]")
+    /**
+     * 物体認識結果を画面に表示する
+     *
+     * @param resultList 物体認識結果
+     */
+    private fun processObjectRecognitionResult(resultList: List<FirebaseVisionObject>) {
+        Log.d(TAG, "[processObjectRecognitionResult] resultList=$resultList")
 
-        if (textureView.isAvailable) {
-            val original = textureView.bitmap
-
-            val matrix = Matrix().apply {
-                val displayDegree = when(textureView.display.rotation) {
-                    Surface.ROTATION_0 -> 0
-                    Surface.ROTATION_90 -> 90
-                    Surface.ROTATION_180 -> 180
-                    Surface.ROTATION_270 -> 270
-                    else -> 0
-                }
-
-                postScale(1f / original.width, 1f / original.height)
-                postTranslate(-0.5f, -0.5f)
-                postRotate(-displayDegree.toFloat())
-                postScale(textureView.width.toFloat(), textureView.height.toFloat())
+        if (resultList.isNotEmpty()) {
+            graphicOverlay.clear()
+            for (result in resultList) {
+                val objectGraphic = ObjectGraphic(graphicOverlay, result)
+                graphicOverlay.add(objectGraphic)
             }
-            val bitmap = Bitmap.createBitmap(original, 0, 0, original.width, original.height, matrix, true)
-
-            // TODO: ここから分岐
-            val visionImage = FirebaseVisionImage.fromBitmap(bitmap)
-
-            val textRecognizer = FirebaseVision.getInstance().onDeviceTextRecognizer
-            textRecognizer.processImage(visionImage)
-                .addOnSuccessListener { texts -> processTextRecognitionResult(texts) }
-                .addOnFailureListener { e -> e.printStackTrace() }
         }
     }
 }
